895fade9737d2e2d338a9e2339f311d9deec0cdb
Ujjwal kumar <149386327+ujjwali2s@users.noreply.github.com>
2024-01-04 20:28:13 +0530

    Delete back directory

--- back/routes/authRoutes.js
+++
@@ -1,9 +0,0 @@
-// backend/routes/authRoutes.js
-const express = require('express');
-const router = express.Router();
-const authController = require('../controllers/authController.js');
-
-router.post('/signup', authController.signup);
-router.post('/login', authController.login);
-router.get('/user', authController.getUser);
-module.exports = router;
