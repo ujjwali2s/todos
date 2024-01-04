f025f9f2934cc416befa8085c58fd033013d4984
Ujjwal kumar <149386327+ujjwali2s@users.noreply.github.com>
2024-01-04 13:35:00 +0530

    Update index.js

--- back/index.js
+++ back/index.js
@@ -16,7 +16,7 @@ app.use(cors({
   // origin:'https://ujjwaltodos.vercel.app'
 }));
 app.get("/",(re,req)=>{
-  re.send("Hell it`s working")
+  res.send("Hell it`s working")
 }
 app.use(express.json());
 
